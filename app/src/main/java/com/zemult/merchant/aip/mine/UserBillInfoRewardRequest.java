000f25801b37f4201ad9b4e736381f02fa695d3f
zhangk <123456>
2017-02-05 10:01:37 +0800

    发红包，收红包接口

---
+++ app/src/main/java/com/zemult/merchant/aip/mine/UserBillInfoRewardRequest.java
@@ -0,0 +1,35 @@
+package com.zemult.merchant.aip.mine;
+
+import android.util.Pair;
+
+import com.google.gson.reflect.TypeToken;
+import com.zemult.merchant.config.Urls;
+import com.zemult.merchant.model.apimodel.APIM_UserBillInfo;
+import com.zemult.merchant.util.Convert;
+
+import java.lang.reflect.Type;
+
+import zema.volley.network.PostStringRequest;
+import zema.volley.network.ResponseListener;
+
+//用户的账单详情-打/赞赏类型的(type=9)
+public class UserBillInfoRewardRequest extends PostStringRequest<Type> {
+
+    public static class Input {
+        public int billId;    //	账单id
+        public String ejson;
+
+
+        public void convertJosn() {
+            ejson = Convert.securityJson(Convert.pairsToJson(
+                    new Pair<String, String>("billId", billId + "")));
+        }
+
+    }
+
+    public UserBillInfoRewardRequest(Input input, ResponseListener listener) {
+        super(Urls.BASIC_URL + Urls.USER_BILL_INFO_REWARD, input.ejson, new TypeToken<APIM_UserBillInfo>() {
+        }.getType(), listener);
+
+    }
+}
