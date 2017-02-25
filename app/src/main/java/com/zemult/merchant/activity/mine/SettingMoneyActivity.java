ed65e74167e66c8474a7d9e5cd1abd7fc2bcd9dc
sunj <123456>
2017-02-25 13:04:25 +0800

    设置金额

--- app/src/main/java/com/zemult/merchant/activity/mine/SettingMoneyActivity.java
+++ app/src/main/java/com/zemult/merchant/activity/mine/SettingMoneyActivity.java
@@ -2,14 +2,17 @@ package com.zemult.merchant.activity.mine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.zemult.merchant.R;
 import com.zemult.merchant.app.BaseActivity;
+import com.zemult.merchant.util.ToastUtil;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -51,14 +54,18 @@ public class SettingMoneyActivity extends BaseActivity {
                 onBackPressed();
                 break;
             case R.id.ok_btn:
-
-                Intent intent = new Intent();
-                //把返回数据存入Intent
-                intent.putExtra("result", ninameEt.getText().toString());
-                //设置返回数据
-                SettingMoneyActivity.this.setResult(RESULT_OK, intent);
+                if (TextUtils.isEmpty(ninameEt.getText().toString())) {
+                    ToastUtil.showMessage("请输入收款金额");
+                } else {
+                    Intent intent = new Intent();
+                    //把返回数据存入Intent
+                    intent.putExtra("result", ninameEt.getText().toString());
+                    //设置返回数据
+                    SettingMoneyActivity.this.setResult(RESULT_OK, intent);
 //                    Toast.makeText(this, "保存成功", Toast.LENGTH_SHORT).show();
-                SettingMoneyActivity.this.finish();
+                    SettingMoneyActivity.this.finish();
+                }
+
 
                 break;
         }
