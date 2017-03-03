7d784502f1ffea94d6485555cd5ae38c9f3608a1
sunj <123456>
2017-03-03 17:18:20 +0800

    赞赏红包

--- app/src/main/java/com/zemult/merchant/activity/mine/TabManageActivity.java
+++ app/src/main/java/com/zemult/merchant/activity/mine/TabManageActivity.java
@@ -193,8 +193,8 @@ public class TabManageActivity extends BaseActivity implements AdapterView.OnIte
             textView.setVisibility(View.GONE);
             xieyiLl.setVisibility(View.VISIBLE);
             applyBtn.setVisibility(View.VISIBLE);
-            applyBtn.setEnabled(false);
-            applyBtn.setBackgroundResource(R.drawable.next_bg_btn_select);
+//            applyBtn.setEnabled(false);
+//            applyBtn.setBackgroundResource(R.drawable.next_bg_btn_select);
 
         }
         if (comefrom == 2) {
@@ -293,13 +293,13 @@ public class TabManageActivity extends BaseActivity implements AdapterView.OnIte
                             }, 50L);
                         }
                     }
-                    if(userAdapter.getCount()>0){
-                        applyBtn.setEnabled(true);
-                        applyBtn.setBackgroundResource(R.drawable.common_selector_btn);
-                    }else{
-                        applyBtn.setEnabled(false);
-                        applyBtn.setBackgroundResource(R.drawable.next_bg_btn_select);
-                    }
+//                    if(userAdapter.getCount()>0){
+//                        applyBtn.setEnabled(true);
+//                        applyBtn.setBackgroundResource(R.drawable.common_selector_btn);
+//                    }else{
+//                        applyBtn.setEnabled(false);
+//                        applyBtn.setBackgroundResource(R.drawable.next_bg_btn_select);
+//                    }
                     break;
                 case R.id.otherGridView:
                     final ImageView moveImageView = getView(view);
