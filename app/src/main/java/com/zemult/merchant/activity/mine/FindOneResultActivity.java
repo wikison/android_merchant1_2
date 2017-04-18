07a20681a75d592eef9f82c212c6ff762282c7b9
sunj <123456>
2017-04-18 13:24:43 +0800

    找人推荐

--- app/src/main/java/com/zemult/merchant/activity/mine/FindOneResultActivity.java
+++ app/src/main/java/com/zemult/merchant/activity/mine/FindOneResultActivity.java
@@ -1,10 +1,13 @@
 package com.zemult.merchant.activity.mine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.zemult.merchant.R;
+import com.zemult.merchant.activity.MainActivity;
 import com.zemult.merchant.app.BaseActivity;
 
 import butterknife.Bind;
@@ -15,6 +18,8 @@ public class FindOneResultActivity extends BaseActivity {
 
     @Bind(R.id.lh_tv_title)
     TextView lhTvTitle;
+    @Bind(R.id.al_btn_login)
+    Button alBtnLogin;
 
     @Override
     public void setContentView() {
@@ -33,7 +38,7 @@ public class FindOneResultActivity extends BaseActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick({R.id.lh_btn_back, R.id.ll_back})
+    @OnClick({R.id.lh_btn_back, R.id.ll_back,R.id.al_btn_login})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.lh_btn_back:
@@ -41,6 +46,12 @@ public class FindOneResultActivity extends BaseActivity {
             case R.id.ll_back:
                 onBackPressed();
                 break;
+            case R.id.al_btn_login:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
         }
     }
+
+
 }
