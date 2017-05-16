725b699abb525e2275fd341b06f12b3d18662b2b
Wikison <wikison@foxmail.com>
2017-05-16 13:10:16 +0800

    bugs

--- app/src/main/java/com/zemult/merchant/im/CreateBespeakNewActivity.java
+++ app/src/main/java/com/zemult/merchant/im/CreateBespeakNewActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
